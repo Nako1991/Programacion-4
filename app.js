ec3999215be05fab2d0f980bfbff5bba0eefd982
Nano <olmos.fernando@hotmail.com>
2024-08-18 13:49:09 -0300

    comenzando ejercicio 10

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 "use strict"
-function test() {
-    console.log("TEST clicked");
-    //document.getElementById("textoEjercicio1").innerText = "hola";
-}
-
 function desplegar(numeroDeEjercicio) {
     let stringEjercicio = "ejercicio" + numeroDeEjercicio;
     const desplegar = document.getElementById(stringEjercicio);
@@ -13,12 +8,11 @@ function desplegar(numeroDeEjercicio) {
 function ejercicio1() {
     desplegar(1);
 
-    let resolucionEjercicio1 = "";
     const arregloNumerico = [12, 5, 8, 21, 34, 7, 18, 29, 42, 11, 6, 15, 24, 31, 48, 9, 14, 33, 50, 27];
-
+    
     const suma = sumarElementosArreglo(arregloNumerico);
-
-    resolucionEjercicio1 = "Arreglo numerico: " + arregloNumerico.toString() + "\n";
+    
+    let resolucionEjercicio1 = "Arreglo numerico: " + arregloNumerico.toString() + "\n";
     resolucionEjercicio1 += "La suma de todos los elementos es de: " + suma + "\n";
 
     document.getElementById("resolucionEjercicio1").innerText = resolucionEjercicio1;
@@ -34,14 +28,13 @@ function sumarElementosArreglo(arregloNumerico) {
 function ejercicio2() {
     desplegar(2);
     
-    let resolucionEjercicio2 = "";
     const arregloNumerico = [12, 5, 8, 21, 34, 7, 18, 29, 42, 11, 6, 15, 24, 31, 48, 9, 14, 33, 50, 27];
-
+    
     const suma = sumarElementosArreglo(arregloNumerico);
     const elementos = arregloNumerico.length;
     const promedio = calcularPromedioArreglo(arregloNumerico);
-
-    resolucionEjercicio2 = "Arreglo numerico: " + arregloNumerico.toString() + "\n";
+    
+    let resolucionEjercicio2 = "Arreglo numerico: " + arregloNumerico.toString() + "\n";
     resolucionEjercicio2 += "La suma de todos los elementos es de: " + suma + "\n";
     resolucionEjercicio2 += "La cantidad de elementos del arreglo es de: " + elementos + "\n";
     resolucionEjercicio2 += "El promedio del arreglo es de: " + promedio + "\n";
@@ -59,12 +52,11 @@ function calcularPromedioArreglo(arregloNumerico) {
 function ejercicio3() {
     desplegar(3);
 
-    let resolucionEjercicio3 = "";
     const arregloNumerico = [12, 5, 8, 21, 34, 7, 18, 29, 42, 11, 6, 15, 24, 31, 48, 9, 14, 33, 50, 27];
     const maximo = encontrarMaximoNumero(arregloNumerico);
     const minimo = encontrarMinimoNumero(arregloNumerico);
-
-    resolucionEjercicio3 = "Arreglo numerico: " + arregloNumerico.toString() + "\n";
+    
+    let resolucionEjercicio3 = "Arreglo numerico: " + arregloNumerico.toString() + "\n";
     resolucionEjercicio3 += "El maximo valor de los elementos es de: " + maximo + "\n";
     resolucionEjercicio3 += "El minimo valor de los elementos es de: " + minimo + "\n";
 
@@ -84,12 +76,11 @@ function encontrarMinimoNumero(arregloNumerico) {
 function ejercicio4() {
     desplegar(4);
 
-    let resolucionEjercicio4 = "";
     const arregloNumerico = [12, 5, 8, 21, 34, 7, 18, 29, 42, 11, 6, 15, 24, 31, 48, 9, 14, 33, 50, 27];
     const valorBuscado = 18;
     const indiceEncontrado = buscarValorNumerico(arregloNumerico, valorBuscado);
 
-    resolucionEjercicio4 = "Arreglo numerico: " + arregloNumerico.toString() + "\n";
+    let resolucionEjercicio4 = "Arreglo numerico: " + arregloNumerico.toString() + "\n";
     resolucionEjercicio4 += "El elemento buscado es: " + valorBuscado + "\n";
     if ( indiceEncontrado != -1 )
         resolucionEjercicio4 += "El indice del elemento buscado es de: " + indiceEncontrado + "\n";
@@ -107,12 +98,11 @@ function buscarValorNumerico(arregloNumerico, valorBuscado) {
 function ejercicio5() {
     desplegar(5);
 
-    let resolucionEjercicio5 = "";
     const arregloNumerico = [12, 5, 8, 21, 34, 7, 18, 29, 42, 11, 6, 15, 24, 31, 48, 9, 14, 33, 50, 27];
     const arregloNumericoPares = filtrarParesArregloNumerico(arregloNumerico);
     const arregloNumericoImpares = filtrarImparesArregloNumerico(arregloNumerico);
 
-    resolucionEjercicio5 = "Arreglo numerico: " + arregloNumerico.toString() + "\n";
+    let resolucionEjercicio5 = "Arreglo numerico: " + arregloNumerico.toString() + "\n";
     resolucionEjercicio5 += "Arreglo numerico pares: " + arregloNumericoPares.toString() + "\n";
     resolucionEjercicio5 += "Arreglo numerico impares: " + arregloNumericoImpares.toString() + "\n";
 
@@ -130,18 +120,105 @@ function filtrarImparesArregloNumerico(arregloNumerico) {
 function ejercicio6() {
     desplegar(6);
 
-    let resolucionEjercicio6 = "";
     const arregloNumerico = [12, 12, 8, 21, 7, 7, 7, 29, 42, 11, 15, 15, 24, 31, 31, 9, 14, 33, 50, 50];
     const arregloNumericoSinDuplicados = filtrarDuplicadosArregloNumerico(arregloNumerico);
 
-    resolucionEjercicio6 = "Arreglo numerico: " + arregloNumerico.toString() + "\n";
+    let resolucionEjercicio6 = "Arreglo numerico: " + arregloNumerico.toString() + "\n";
     resolucionEjercicio6 += "Arreglo numerico sin duplicados: " + arregloNumericoSinDuplicados.toString() + "\n";
 
     document.getElementById("resolucionEjercicio6").innerText = resolucionEjercicio6;
 }
 
 function filtrarDuplicadosArregloNumerico(arregloNumerico) {
-    let arregloNumericoSinDuplicados = [];
-    
+    const arregloNumericoSinDuplicados = [... new Set(arregloNumerico)];
     return arregloNumericoSinDuplicados;
+}
+
+function ejercicio7() {
+    desplegar(7);
+
+    const arregloNumerico = [12, 5, 8, 21, 34, 7, 18, 29, 42, 11, 6, 15, 24, 31, 48, 9, 14, 33, 50, 27];
+    const arregloNumericoOrdenado = ordenarArregloNumerico(arregloNumerico);
+
+    let resolucionEjercicio7 = "Arreglo numerico: " + arregloNumerico.toString() + "\n";
+    resolucionEjercicio7 += "Arreglo numerico ordenado: " + arregloNumericoOrdenado.toString() + "\n";
+
+    document.getElementById("resolucionEjercicio7").innerText = resolucionEjercicio7;
+}
+
+function ordenarArregloNumerico(arregloNumerico) {
+    const arregloNumericoOrdenado = arregloNumerico.slice(); //slice crea una copia para no modificar el original
+    return arregloNumericoOrdenado.sort( (a, b) => a - b );
+}
+
+function ejercicio8() {
+    desplegar(8);
+
+    const arregloNumerico = [12, 12, 8, 21, 7, 7, 7, 29, 42, 11, 15, 15, 24, 31, 31, 9, 14, 33, 50, 50];
+    const arregloNumericoSinEliminados = eliminarValorArregloNumerico(arregloNumerico, 7);
+
+    let resolucionEjercicio8 = "Arreglo numerico: " + arregloNumerico.toString() + "\n";
+    resolucionEjercicio8 += "Numero a eliminar: 7" + "\n";
+    resolucionEjercicio8 += "Arreglo numerico ordenado: " + arregloNumericoSinEliminados.toString() + "\n";
+
+    document.getElementById("resolucionEjercicio8").innerText = resolucionEjercicio8;
+}
+
+function eliminarValorArregloNumerico(arregloNumerico, numeroAEliminar) {
+    let arregloNumericoSinEliminados = [];
+    let i = 0;
+    for ( let numero of arregloNumerico ) {
+        if ( arregloNumerico[i] != numeroAEliminar )
+            arregloNumericoSinEliminados.push(numero);
+        i++;
+    }
+    return arregloNumericoSinEliminados;
+}
+
+function ejercicio9() {
+    desplegar(9);
+
+    const arregloNumerico1 = [12, 6, 8, 21, 7, 9, 14, 33, 50, 50];
+    const arregloNumerico2 = [12, 5, 8, 21, 34, 7, 18, 29, 42, 11];
+    let arregloCombinadoSinDuplicados = [... new Set(arregloNumerico1), ... new Set(arregloNumerico2)];
+    arregloCombinadoSinDuplicados = filtrarDuplicadosArregloNumerico(arregloCombinadoSinDuplicados);
+
+    let resolucionEjercicio9 = "Arreglo numerico 1: " + arregloNumerico1.toString() + "\n";
+    resolucionEjercicio9 += "Arreglo numerico 2: " + arregloNumerico2.toString() + "\n";
+    resolucionEjercicio9 += "Arreglo numerico combinado: " + arregloCombinadoSinDuplicados.toString() + "\n";
+
+    document.getElementById("resolucionEjercicio9").innerText = resolucionEjercicio9;
+}
+
+function ejercicio10() {
+    desplegar(10);
+
+    const listaDeCompras = new ListaDeCompras();
+    const producto1 = new Producto("Arroz", 5);
+    listaDeCompras.agregarProducto(producto1);
+     
+    console.log(listaDeCompras);
+    console.log(producto1);
+    listaDeCompras.mostrarLista();
+}
+
+function ListaDeCompras() {
+    this.lista = [];
+    this.agregarProducto = function (producto) {
+        this.lista.push(producto);
+    }
+    this.mostrarLista = function () {
+        console.log("Lista de compras:");
+        for ( let producto of this.lista )
+            producto.mostrar();
+    }
+}
+
+function Producto(nombre, stock) {
+    this.nombre = nombre;
+    this.stock = stock;
+    this.mostrar = function () {
+        console.log(this.nombre);
+        console.log(this.stock);
+    }
 }
