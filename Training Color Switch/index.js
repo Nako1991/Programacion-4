11e773bb63ef8199d3299be4f679d0c19bd0841c
Nano <olmos.fernando@hotmail.com>
2024-08-20 19:57:14 -0300

    color switch completado

--- Training Color Switch/index.js
+++ Training Color Switch/index.js
@@ -1,1 +1,15 @@
 "use strict"
+
+function switchBackgroundColor(color) {
+    const body = document.getElementsByTagName("body")[0];
+    if ( color != "random" )
+        body.style.backgroundColor = color;
+    else {
+        const r = Math.round(Math.random() * 256);
+        const g = Math.round(Math.random() * 256);
+        const b = Math.round(Math.random() * 256);
+        const colorRandom = `rgb(${r}, ${g}, ${b})`;
+        body.style.backgroundColor = colorRandom;
+    }
+}
+
